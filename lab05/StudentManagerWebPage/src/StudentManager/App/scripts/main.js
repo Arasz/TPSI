603461a9912b817a2d4ce543526364a8e78c73f7
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-06-13 11:01:54 +0200

    Add view model backend to all tables

---
+++ lab05/StudentManagerWebPage/src/StudentManager/App/scripts/main.js
@@ -0,0 +1,8 @@
+﻿requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'scripts/',
+});
+
+require(['dependencies/jquery', 'dependencies/knockout', 'mainViewModel', 'dependencies/domReady!'], function ($, ko, mainViewModel) {
+    ko.applyBindings(new mainViewModel());
+});
--- lab05/StudentManagerWebPage/src/StudentManager/Startup.cs
+++ lab05/StudentManagerWebPage/src/StudentManager/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,11 +7,8 @@ namespace StudentManager
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
@@ -24,11 +17,14 @@ namespace StudentManager
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.SendFileAsync("MainView.html");
             });
         }
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
     }
 }
